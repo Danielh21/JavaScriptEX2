85f2868ce893d0a834e4b080b4c68d7c5fbc2e56
Daniel <danielhollmann@hotmail.com>
2017-02-08 15:41:19 +0100

    Uncommendted the import functions

--- EX8.js
+++ EX8.js
@@ -1,14 +1,14 @@
 /**
  * Created by daniel on 08-02-2017.
 //  */
-import {f} from './MyModule/module1.js';
-
-console.log(f(2,3,true,5,"hej",[2,4,3],new Date(),{}));
-
-
-// const functions = require('./MyModule/module1.js');
+// import {f} from './MyModule/module1.js';
 //
-// console.log(functions.func(2,3,true,5,"hej",[2,4,3],new Date(),{}));
+// console.log(f(2,3,true,5,"hej",[2,4,3],new Date(),{}));
+
+
+const functions = require('./MyModule/module1.js');
+
+console.log(functions.func(2,3,true,5,"hej",[2,4,3],new Date(),{}));
 
 
 
--- MyModule/module1.js
+++ MyModule/module1.js
@@ -6,20 +6,7 @@
 
 
 
-export function f(x,y,bool,z,stringS,arrayNum,date, obj){
-    return `
-Sum: ${x + y}
-rest value 1 is a: ${bool.constructor.name}
-rest value 2 is a: ${z.constructor.name}
-rest value 3 is a: ${stringS.constructor.name}
-rest value 4 is a: ${arrayNum.constructor.name}
-rest value 5 is a: ${date.constructor.name}
-rest value 6 is a: ${obj.constructor.name}
-`
-};
-
-
-// module.exports.func = function(x,y,bool,z,stringS,arrayNum,date, obj){
+// export function f(x,y,bool,z,stringS,arrayNum,date, obj){
 //     return `
 // Sum: ${x + y}
 // rest value 1 is a: ${bool.constructor.name}
@@ -30,3 +17,16 @@ rest value 6 is a: ${obj.constructor.name}
 // rest value 6 is a: ${obj.constructor.name}
 // `
 // };
+
+
+module.exports.func = function(x,y,bool,z,stringS,arrayNum,date, obj){
+    return `
+Sum: ${x + y}
+rest value 1 is a: ${bool.constructor.name}
+rest value 2 is a: ${z.constructor.name}
+rest value 3 is a: ${stringS.constructor.name}
+rest value 4 is a: ${arrayNum.constructor.name}
+rest value 5 is a: ${date.constructor.name}
+rest value 6 is a: ${obj.constructor.name}
+`
+};
